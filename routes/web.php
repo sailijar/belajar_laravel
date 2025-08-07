f56aa96cd3009dfcc3c438beab315f307d065877
sailijar <sailijar88@gmail.com>
2025-08-07 12:46:13 +0700

    membuat tabel dosen , bagian tambah ,edit dan hapus

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,19 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/cektemplate', function(){
+     return view('layouts.template');
+ });
+
+ Route::get('/dosen', function(){
+     return view('dosen.index');
+ });
+
+Route::get('/dosen', [DosenController::class, 'index']);
+Route::get('/dosen/form', [DosenController::class, 'create']);
+Route::post('/dosen', [DosenController::class, 'store']);
+Route::get('/dosen/edit/{id}', [DosenController::class, 'edit']);
+Route::put('/dosen/{id}', [DosenController::class, 'update']);
+Route::delete('/dosen/{id}', [DosenController::class, 'destroy']);
+
